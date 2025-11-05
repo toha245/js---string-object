4240ec70185d06674a527ebab1210ca63ad47454
toha245 <mdabdulawal2001@gmail.com>
2025-11-05 22:43:17 +0600

    fourth commit

--- concat.js
+++ concat.js
@@ -5,6 +5,7 @@ const last = 'nabid';
 const fullName = first + ' ' + last;
 // console.log(fullName);
 const fullName2 = first.concat(' ') .concat(last);
+// const fullName2 = first.concat(' ', last); concat bar bar lekheo kora jay, abar 1 bar lekhe koma diye diye o kora jay.
 console.log(fullName2);
 
 // includes
